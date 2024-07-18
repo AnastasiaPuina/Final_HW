dc7d37c0db188d1d42d5c78a21d16f6440cd096e
Anastasia Pulina <yanssu@mail.ru>
2024-07-18 14:56:09 +0300

    Added ArrayFilter method

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿string[] ArrayFilter(string[] array)
+{
+    // Определяем длину конечного массива
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+// Записываем информацию в новый массив
+    string[] result = new string[count]; 
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[j] = array[i];
+            j++;
+        }
+    }
+    return result;
+}
